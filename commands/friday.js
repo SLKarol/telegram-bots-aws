732166e9df2f38c4a0cebd0969097b76a31352e4
Stanislav Karol <slsinitsin@gmail.com>
2021-03-14 15:12:52 +0300

    refactor event send message

--- commands/friday.js
+++ commands/friday.js
@@ -1,4 +1,10 @@
-const { getNewRecords, mapRedditForTelegram } = require("../lib/reddit");
+const {
+  getNewRecords,
+  filterAllowContent,
+  prepareRecords,
+  checkCorrectImages,
+  getPartsMessage,
+} = require("../lib/reddit");
 const { delay } = require("../lib/common");
 
 /**
@@ -9,95 +15,88 @@ const { delay } = require("../lib/common");
  * @param {number} prop.chatId
  */
 const friday = async ({ telegram, chatId }) => {
-  // Надпись
-  await telegram.sendMessage(
+  // Надпись о начале работы
+  telegram.sendMessage(
     chatId,
     "Готовятся материалы для публикации в этом чате."
   );
 
+  getNewRecords(20, responseHandler);
+
   /**
-   * Собранные выпуски.
-   * Когда не-пятница, их вдвое меньше.
+   * Обработчик ответа Reddit
+   * @param {object} response Ответ Reddit'a
    */
-  const friDay = await getNewRecords(20);
-
-  //массив, в который будет выведен результат.
-  let fridayMessages = [];
-  const size = 10;
-  // Получить массив из частей по size штук
-  for (let i = 0; i < Math.ceil(friDay.length / size); i++) {
-    fridayMessages[i] = friDay
-      .slice(i * size, i * size + size)
-      // Подготовить эти 10 записей к отправке в телеграм
-      .map(mapRedditForTelegram);
+  async function responseHandler(response) {
+    const {
+      data: { children },
+    } = response;
+    let filteredRecords = filterAllowContent(prepareRecords(children));
+    checkCorrectImages(filteredRecords, sendFilteredRecords);
   }
-  //--- Примеры отправки сообщения не через библиотеку
-  // 1 example
-  // let url = new URL(
-  //   `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN_FRIDAY}/sendMediaGroup`
-  // );
-  // const params = { chat_id: chatId };
-  // for (group of fridayMessages) {
-  //   params.media = JSON.stringify(group);
-  //   url.search = new URLSearchParams(params).toString();
-  //   try {
-  //     await fetch(url);
-  //     await delay();
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  // }
-  // 2 example
-  // let url = new URL(
-  //   `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN_FRIDAY}/sendPhoto`
-  // );
-  // const params = { chat_id: chatId, disable_notification: true };
-  // for (photo of friDay) {
-  //   params.photo = photo.url;
-  //   params.caption = photo.title;
-  //   url.search = new URLSearchParams(params).toString();
-  //   try {
-  //     await fetch(url);
-  //     await delay();
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  // }
-  // Отправить фото сгруппированными
-  for (group of fridayMessages) {
-    if (group.length > 1) {
-      try {
-        await telegram.sendMediaGroup(chatId, group, {
-          disable_notification: true,
-        });
-      } catch (e) {
-        console.error(e);
+
+  /**
+   * Отправить сообщения в телеграм
+   * @param {Array} records
+   */
+  async function sendFilteredRecords(records) {
+    const fridayMessages = getPartsMessage(
+      records.filter((record) => record.correct)
+    );
+    for (group of fridayMessages) {
+      if (group.length > 1) {
+        try {
+          await telegram.sendMediaGroup(chatId, group, {
+            disable_notification: true,
+          });
+        } catch (e) {
+          console.error(e);
+        }
+      } else {
+        const [photo] = group;
+        telegram.sendPhoto(
+          chatId,
+          { url: photo.url },
+          { caption: photo.title, disable_notification: true }
+        );
       }
-    } else {
-      const [photo] = group;
-      telegram.sendPhoto(
-        chatId,
-        { url: photo.url },
-        { caption: photo.title, disable_notification: true }
-      );
+      await delay();
     }
-    await delay();
+    telegram.sendMessage(chatId, "Задача выполнена.");
   }
-  // Отправить по одной фото
-  // for (photo of friDay) {
-  //   try {
-  //     await telegram.sendPhoto(chatId, {
-  //       source: photo.content,
-  //       caption: photo.title,
-  //       disable_notification: true,
-  //     });
-  //     await delay(1234);
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  // }
-
-  telegram.sendMessage(chatId, "Задача выполнена.");
 };
 
 module.exports = friday;
+
+//--- Примеры отправки сообщения не через библиотеку
+// 1 example
+// let url = new URL(
+//   `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN_FRIDAY}/sendMediaGroup`
+// );
+// const params = { chat_id: chatId };
+// for (group of fridayMessages) {
+//   params.media = JSON.stringify(group);
+//   url.search = new URLSearchParams(params).toString();
+//   try {
+//     await fetch(url);
+//     await delay();
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
+// 2 example
+// let url = new URL(
+//   `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN_FRIDAY}/sendPhoto`
+// );
+// const params = { chat_id: chatId, disable_notification: true };
+// for (photo of friDay) {
+//   params.photo = photo.url;
+//   params.caption = photo.title;
+//   url.search = new URLSearchParams(params).toString();
+//   try {
+//     await fetch(url);
+//     await delay();
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
